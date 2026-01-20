04745e071abddf96094dde09163152568633313b
prasitex4-source <prasitex4@gmail.com>
2026-01-20 21:28:11 +0100

    meow

--- Assets/Scripts/Inventario/Item.cs
+++ Assets/Scripts/Inventario/Item.cs
@@ -2,18 +2,23 @@ using System;
 using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 
 [CreateAssetMenu(menuName = "Item")]
 public class Item : ScriptableObject
 {
+
+    [Header("Gameplay")]
+    public TileBase tile;
+    public ItemType type;
+    public ActionType actionType;
+    public Vector2Int range = new Vector2Int(5, 4);
+
     [Header("UI")]
     public Sprite image;
     public bool stackable = true; //por si hay q acumular objetos
 
-    [Header("Gameplay")]
-    public ItemType type;
-    public ActionType actionType;
     
 }
 
--- Assets/Scripts/SlotInventario.cs
+++ Assets/Scripts/SlotInventario.cs
@@ -1,8 +1,25 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SlotInventario : MonoBehaviour, IDropHandler
 {
+    public Image image;
+    public Color selected, notselected;
+
+    private void wake()
+    {
+        Deselect();
+    }
+
+    public void Select()
+    {
+        image.color = selected;
+    }
+    public void Deselect()
+    {
+        image.color = notselected;
+    }
     public void OnDrop(PointerEventData eventData)
     {
         GameObject dropped = eventData.pointerDrag;
