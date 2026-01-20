617d9fc033d77eae0894d5f366881fd3dd022544
prasitex4-source <prasitex4@gmail.com>
2026-01-20 19:16:49 +0100

    enemigos

---
+++ Assets/Scripts/Entities/Enemmies/EnemyFollow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    private Transform target;
+    public float speed;
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
